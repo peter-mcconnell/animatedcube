4236190678671ffcdc72113e04dfdabfdd587073
Peter McConnell <me@petermcconnell.com>
2022-12-19 15:47:01 +0000

    improvements

--- animated_cube.c
+++ animated_cube.c
@@ -1,40 +1,121 @@
+#include <stdlib.h>
+#include <math.h>
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
 #include <GL/glut.h>
-#include <stdio.h>
+#endif
 
-#define WINDOW_WIDTH 500
-#define WINDOW_HEIGHT 500
+void DisplayFunc(void)
+{
+  static float alpha = 0;
 
-float angle = 0.0f;
-
-void display() {
+  /* Clear the buffer, clear the matrix */
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glLoadIdentity();
-  glTranslatef(0.0f, 0.0f, -5.0f);
-  glRotatef(angle, 1.0f, 1.0f, 1.0f);
 
-  glutSolidCube(2.0f);
+  /* A step backward, then spin the cube */
+  glTranslatef(0, 0, -10);
+  glRotatef(30, 1, 0, 0);
+  glRotatef(alpha, 0, 1, 0);
 
+  /* We tell we want to draw quads */
+  glBegin(GL_QUADS);
+
+  /* Every four calls to glVertex, a quad is drawn */
+  glColor3f(0, 0, 0); glVertex3f(-1, -1, -1);
+  glColor3f(0, 0, 1); glVertex3f(-1, -1,  1);
+  glColor3f(0, 1, 1); glVertex3f(-1,  1,  1);
+  glColor3f(0, 1, 0); glVertex3f(-1,  1, -1);
+
+  glColor3f(1, 0, 0); glVertex3f( 1, -1, -1);
+  glColor3f(1, 0, 1); glVertex3f( 1, -1,  1);
+  glColor3f(1, 1, 1); glVertex3f( 1,  1,  1);
+  glColor3f(1, 1, 0); glVertex3f( 1,  1, -1);
+
+  glColor3f(0, 0, 0); glVertex3f(-1, -1, -1);
+  glColor3f(0, 0, 1); glVertex3f(-1, -1,  1);
+  glColor3f(1, 0, 1); glVertex3f( 1, -1,  1);
+  glColor3f(1, 0, 0); glVertex3f( 1, -1, -1);
+
+  glColor3f(0, 1, 0); glVertex3f(-1,  1, -1);
+  glColor3f(0, 1, 1); glVertex3f(-1,  1,  1);
+  glColor3f(1, 1, 1); glVertex3f( 1,  1,  1);
+  glColor3f(1, 1, 0); glVertex3f( 1,  1, -1);
+
+  glColor3f(0, 0, 0); glVertex3f(-1, -1, -1);
+  glColor3f(0, 1, 0); glVertex3f(-1,  1, -1);
+  glColor3f(1, 1, 0); glVertex3f( 1,  1, -1);
+  glColor3f(1, 0, 0); glVertex3f( 1, -1, -1);
+
+  glColor3f(0, 0, 1); glVertex3f(-1, -1,  1);
+  glColor3f(0, 1, 1); glVertex3f(-1,  1,  1);
+  glColor3f(1, 1, 1); glVertex3f( 1,  1,  1);
+  glColor3f(1, 0, 1); glVertex3f( 1, -1,  1);
+
+  /* No more quads */
+  glEnd();
+
+  /* Rotate a bit more */
+  alpha = alpha + 0.1;
+
+  /* End */
   glFlush();
   glutSwapBuffers();
-}
 
-void idle() {
-  angle += 0.1f;
-  if (angle > 360.0f) {
-    angle -= 360.0f;
-  }
+  /* Update again and again */
   glutPostRedisplay();
 }
 
-int main(int argc, char **argv) {
+/*
+** Function called when the window is created or resized
+*/
+void ReshapeFunc(int width, int height)
+{
+  glMatrixMode(GL_PROJECTION);
+
+  glLoadIdentity();
+  gluPerspective(20, width / (float) height, 5, 15);
+  glViewport(0, 0, width, height);
+
+  glMatrixMode(GL_MODELVIEW);
+  glutPostRedisplay();
+}
+
+/*
+** Function called when a key is hit
+*/
+void KeyboardFunc(unsigned char key, int x, int y)
+{
+  int foo;
+
+  foo = x + y; /* Has no effect: just to avoid a warning */
+  if ('q' == key || 'Q' == key || 27 == key)
+      exit(0);
+}
+
+
+int main(int argc, char **argv)
+{
+  /* Creation of the window */
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-  glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-  glutCreateWindow("Animated Cube");
-  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+  glutInitWindowSize(500, 500);
+  glutCreateWindow("Spinning cube");
+
+  /* OpenGL settings */
+  glClearColor(0, 0, 0, 0);
   glEnable(GL_DEPTH_TEST);
-  glutDisplayFunc(display);
-  glutIdleFunc(idle);
+
+  /* Declaration of the callbacks */
+  glutDisplayFunc(&DisplayFunc);
+  glutReshapeFunc(&ReshapeFunc);
+  glutKeyboardFunc(&KeyboardFunc);
+
+  /* Loop */
   glutMainLoop();
+
+  /* Never reached */
   return 0;
 }
